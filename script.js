663c9cf00d0d1f6e9a02fe47c56a4da4d92340bf
mohammed kaif <mkaif555123@gmail.com>
2024-06-02 21:58:10 +0500

    events 10% completed

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-//Events Means UserInterFace 
+// type , timeStamp , defaultedPrevented , 
+//target , toElement , srcElement , currentTarget,
+//clientX , clientY , screenX , screenY,
+//altKey , ctrlKey , shiftKey , keyCode
+
 
 // let par = document.getElementById("par");
 
 // par.addEventListener("mousemove"  , () => {
 //     console.log("kaif");
 // })
+
