2f3be9c046b831d27167c5861c00231f86c58f42
mohammed kaif <mkaif555123@gmail.com>
2024-06-03 21:39:55 +0500

    eventlistner true false capture

--- script.js
+++ script.js
@@ -4,8 +4,16 @@
 //altKey , ctrlKey , shiftKey , keyCode
 
 
-// let par = document.getElementById("par");
+let p1 = document.getElementById("p1");
+let main = document.getElementById("main")
 
-// par.addEventListener("mousemove"  , () => {
-//     console.log("kaif");
-// })
+main.addEventListener("click" , () =>{
+    console.log("div clicked");
+} , true)
+
+p1.addEventListener("click"  , () => {
+    console.log("paragraph clicked");
+} , true)
+
+//eventListner ke andar true value top se capture krti hai
+//aur false ki value bottom se capture krti hai
